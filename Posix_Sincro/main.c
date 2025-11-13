54a818de28cf3dbd96da9616f500456a3caf2997
Thomasu L. <108015943+Tho0x2B@users.noreply.github.com>
2025-11-13 15:17:46 -0500

    Add multi-threading support with producers

--- Posix_Sincro/main.c
+++ Posix_Sincro/main.c
@@ -23,6 +23,7 @@
 
 /* ---- Buffer circular de impresion: MAX_BUFFERS entradas de 100 chars ---- */
 char buf [MAX_BUFFERS] [100];
+
 /* ---- indice de escritura y de lectura (spooler) sobre el buffer circular ---- */
 int buffer_index;
 int buffer_print_index;
@@ -31,6 +32,7 @@ int buffer_print_index;
 pthread_mutex_t buf_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t buf_cond = PTHREAD_COND_INITIALIZER;
 pthread_cond_t spool_cond = PTHREAD_COND_INITIALIZER;
+
 /* ---- Contabilidad: buffers libres y lineas pendientes por imprimir ---- */
 int buffers_available = MAX_BUFFERS;
 int lines_to_print = 0;
@@ -46,41 +48,60 @@ int lines_to_print = 0;
  *   - int: codigo de salida del programa (0 si todo fue correcto).                             *
  ************************************************************************************************/
 int main (int argc, char **argv){
+    
     /* ---- Identificadores de hilos: 10 productores y 1 spooler ---- */
     pthread_t tid_producer [10], tid_spooler;
+    
     /* ---- Variables auxiliares: i para bucles, r para codigos de retorno ---- */
     int i, r;
 
     /* ---- Inicializar indices del buffer circular ---- */
     buffer_index = buffer_print_index = 0;
+    
     /* ---- Crear hilo spooler (consumidor/imprimiendo lineas) ---- */
     if ((r = pthread_create (&tid_spooler, NULL, spooler, NULL)) != 0) {
+        
         /* ---- Reportar error si falla la creacion del spooler ---- */
         fprintf (stderr, "Error = %d (%s)\n", r, strerror (r)); exit (1);
+        
     }
     /* ---- Identificadores logicos para productores (0..9) ---- */
     int thread_no [10];
+    
     /* ---- Crear los 10 hilos productores, pasando su id por argumento ---- */
     for (i = 0; i < 10; i++) {
+        
         thread_no [i] = i;
+        
         if ((r = pthread_create (&tid_producer [i], NULL, producer, (void *) &thread_no [i])) != 0) {
+            
             /* ---- Reportar error si falla la creacion de algun productor ---- */
             fprintf (stderr, "Error = %d (%s)\n", r, strerror (r)); exit (1);
+            
         }
+    
     }
     /* ---- Esperar a que terminen todos los productores ---- */
     for (i = 0; i < 10; i++)
+    {       
+        
         if ((r = pthread_join (tid_producer [i], NULL)) == -1) {
+            
             /* ---- Reportar error si falla el join de algun productor ---- */
             fprintf (stderr, "Error = %d (%s)\n", r, strerror (r)); exit (1);
         }
+        
+    }
     
     /* ---- Esperar a que el spooler vacie todas las lineas pendientes ---- */
     while (lines_to_print) sleep (1);
+    
     /* ---- Cancelar el hilo spooler (ya no hay trabajo) ---- */
     if ((r = pthread_cancel (tid_spooler)) != 0) {
+        
         /* ---- Reportar error si falla la cancelacion del spooler ---- */
         fprintf (stderr, "Error = %d (%s)\n", r, strerror (r)); exit (1);
+        
     }
 
     /* ---- Finalizacion correcta del proceso principal ---- */
