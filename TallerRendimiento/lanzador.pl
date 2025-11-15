6274936b865de56143df5f5d21b087c159f6aa71
Thomasu L. <108015943+Tho0x2B@users.noreply.github.com>
2025-11-14 21:14:18 -0500

    Update file handling and output capture in lanzador.pl
    Refactor file handling to use append mode and capture output.

--- TallerRendimiento/lanzador.pl
+++ TallerRendimiento/lanzador.pl
@@ -12,19 +12,28 @@ $Path = `pwd`;
 chomp($Path);
 
 $Nombre_Ejecutable = "mmClasicaFork";
-@Size_Matriz = ("2","10","20");
-@Num_Hilos = (1,2,4,8,16,20);
+@Size_Matriz  = ("2","10","20");
+@Num_Hilos    = (1,2,4,8,16,20);
 $Repeticiones = 4;
 
-foreach $size (@Size_Matriz){
-	foreach $hilo (@Num_Hilos) {
-		$file = "$Path/$Nombre_Ejecutable-".$size."-Hilos-".$hilo.".dat";
-		for ($i=0; $i<$Repeticiones; $i++) {
-#system("$Path/$Nombre_Ejecutable $size $hilo  >> $file");
-			printf("$Path/$Nombre_Ejecutable $size $hilo \n");
-            system("$Path/$Nombre_Ejecutable $size $hilo >> $file");
-		}
-		close($file);
-	$p=$p+1;
-	}
+foreach $size (@Size_Matriz) {
+    foreach $hilo (@Num_Hilos) {
+
+        $file = "$Path/$Nombre_Ejecutable-".$size."-Hilos-".$hilo.".dat";
+
+        # Abrir archivo en modo append (agregar al final)
+        open(FH, ">>", $file) or die "No se puede abrir $file: $!";
+
+        for ($i = 0; $i < $Repeticiones; $i++) {
+            # Ejecutar el programa y capturar la salida
+            $salida = `$Path/$Nombre_Ejecutable $size $hilo`;
+
+            # Escribir la salida en el archivo
+            print FH $salida;
+        }
+
+        # Cerrar el archivo
+        close(FH);
+    }
 }
+
